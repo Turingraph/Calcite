47bebf9db8a3172d3dff7e1259e840cea3c45f1c
Turingraph <nemocheche@gmail.com>
2025-02-01 12:06:06 +0700

    Create frontend/color_ui

---
+++ frontend/src/basic_ui/color_ui/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import color_ui_type from "./type";
+import Int_to_rgb from "../../hex_rgb/int_to_rgb";
+
+export default function Color_ui({
+    body,
+    rgb=255
+}:color_ui_type
+){
+    return <div style={{backgroundColor:Int_to_rgb({input:rgb})}}>{body}</div>
+}
---
+++ frontend/src/basic_ui/color_ui/type.tsx
@@ -0,0 +1,8 @@
+import React, {JSX} from "react";
+
+type color_ui_type = {
+    body:JSX.Element,
+    rgb?:undefined|number|number[]
+}
+
+export default color_ui_type;
