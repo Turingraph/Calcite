45b6724c3d0b08c516374e1627a53ac97547a676
Turingraph <nemocheche@gmail.com>
2025-02-10 10:13:15 +0700

    Change folder structure of frontend/components -> frontend/ui

---
+++ frontend/src/ui/code_examples/keyof.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+// https://www.freecodecamp.org/news/typescript-generics-with-functional-react-components/
+// https://www.w3schools.com/typescript/typescript_keyof.php
+
+// https://stackoverflow.com/questions/76755226/
+// include-information-about-the-values-type-in-keyof-type
+
+export function Summary<t extends object, k extends keyof t, j>({
+  data,
+  property,
+  input
+}:{
+  data:Record<k, j>,
+  property:k
+  input:j
+}) {
+  const value = data[property] as string;
+  data[property] = input;
+
+  return (
+    <div>
+      {(typeof property === "string") ? (
+        <p>
+          {property}: {value}{" "}
+        </p>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+  
