17c072f5b91460988e141c23d8d703abec108d4e
Turingraph <nemocheche@gmail.com>
2025-02-10 12:41:22 +0700

    minor modification

--- frontend/src/components/button/history_buttons.tsx
+++ frontend/src/components/button/history_buttons.tsx
@@ -3,32 +3,6 @@ import * as a from "../../type/alias";
 import Click_button from "./click_button";
 import Input_opt from "../search/input_opt";
 
-/*
-any = any type
-unknown = unknown type
-
-any allow the user to use the any type variable in any unrestricted way.
-
-unknown treat the unknown type variable as variable with unknown type and only
-allow the user to use it such that it is consistence with the first initialized 
-value.
-
-```
-let vAny: any = 10;          // We can assign anything to any
-let vUnknown: unknown =  10; // We can assign anything to unknown just like any 
-
-
-let s1: string = vAny;     // Any is assignable to anything 
-let s2: string = vUnknown; // Invalid; we can't assign vUnknown to any other type (without an explicit assertion)
-
-vAny.method();     // Ok; anything goes with any
-vUnknown.method(); // Not ok; we don't know anything about this variable
-```
-
-Reference
-*   https://stackoverflow.com/questions/51439843/unknown-vs-any
-*/
-
 export default function History_buttons<t>({
     history,
 }:{history:a.use_state_t<a.history<t>>}
