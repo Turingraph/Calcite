9eef1dd10e918808b879e843669cb88f0a240116
Turingraph <nemocheche@gmail.com>
2025-02-28 17:14:10 +0700

    minor modification

--- frontend/src/layout/box/box_edit.tsx
+++ frontend/src/layout/box/box_edit.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import * as a from "../../type/alias"
+import OBJ_SELF from "../../components/obj/obj_self";
+import { DEFAULT_BOX } from "../../data/default_config";
+import { num_to_255, num_to_size } from "../../utility/convert";
 
 export default function BOX_EDIT({
     //
--- frontend/src/layout/ocr/ocr_config.tsx
+++ frontend/src/layout/ocr/ocr_config.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import * as a from "../../type/alias"
+import FILE_EXPORT from "../../components/file/file_export";
+import FILE_IMPORT from "../../components/file/file_import";
 
 export default function OCR_CONFIG({
     //
