b45ed14c10366c2962ffce556b99149d744f3d51
Turingraph <nemocheche@gmail.com>
2025-03-06 15:37:57 +0700

    Change name from hook/ to use_reducer/

---
+++ frontend/src/src/use_reducer/act_arr.tsx
@@ -0,0 +1,107 @@
+import { sort_arr } from "./func_arr";
+import * as oarr from "./func_objarr"
+
+export type act_arr_t<t> = {
+    sort?:"NO_SORT"|"SORT"|"REVERSE"|undefined
+} & ({
+    type:"SORT"
+} | {
+    type:"PUSH",
+    input:t
+}
+| {
+    type:"DELETE",
+    index:number
+}
+| {
+    type:"EDIT",
+    index:number,
+    input:t,
+}
+| {
+    type:"SET",
+    input: t[],
+})
+export default function act_arr<t>
+    (prev_arr:t[], action:act_arr_t<t>){
+    switch(action.type) { 
+        case "SORT": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_SORT_ARR = sort_arr(
+                C_COPY_ARR,
+                action.sort)
+            return C_SORT_ARR
+        } 
+        case "EDIT": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_UPDATE = oarr.edit_item(
+                C_COPY_ARR,
+                action.index,
+                action.input
+            )
+            const C_SORT_ARR = sort_arr(
+                C_UPDATE,
+                action.sort
+            )
+            return C_SORT_ARR
+        } 
+        case "PUSH": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_UPDATE = oarr.push_arr(
+                C_COPY_ARR,
+                action.input
+            )
+            const C_SORT_ARR = sort_arr(
+                C_UPDATE,
+                action.sort
+            )
+            return C_SORT_ARR
+        } 
+        case "DELETE": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_UPDATE = oarr.delete_item(
+                C_COPY_ARR,
+                action.index
+            )
+            const C_SORT_ARR = sort_arr(
+                C_UPDATE,
+                action.sort
+            )
+            return C_SORT_ARR
+        } 
+        case "SET": {
+            const C_SORT_ARR = sort_arr(
+                action.input,
+                action.sort
+            )
+            return C_SORT_ARR
+        }
+        default: { 
+           console.log("--------------------------------------------------------------------")
+           console.log("The action.type of useArr is invalid.")
+           console.log("The action.type should be \"SORT\"|\"PUSH\"|\"DELETE\"|\"EDIT\"|\"SET\"")
+           console.log("Warning from frontend/ src/ src/ hook/ useObjArr.tsx/ function reducer")
+           console.log("--------------------------------------------------------------------")
+           const C_COPY_ARR = [...prev_arr]
+           return C_COPY_ARR
+        } 
+    }
+}
+
+// export default function useArr<t>(init:t[]){
+//     const [ss_arr, setss_arr] = useReducer(reducer, init);
+//     return [ss_arr, setss_arr]
+// }
+
+/*
+https://stackoverflow.com/questions/69678018/
+i-wrote-a-custom-state-hook-using-usereducer-is-it-dangerous-to-use-react-ts
+
+A "reducer" is a function that takes the previous state and an "action" and returns a new state. 
+The action meant to be an instruction rather a state.
+*/
+
+export type use_arr_t<t> = {
+    ss:t[],
+    setss:React.ActionDispatch<[action: act_arr_t<t>]>
+}
---
+++ frontend/src/src/use_reducer/act_objarr.tsx
@@ -0,0 +1,157 @@
+import * as oarr from "./func_objarr"
+import * as a from "../type/alias"
+
+// Learn Custom Hooks In 10 Minutes
+// https://youtu.be/6ThXsUwLWvc?si=TOVkyJuod3AuQxMS
+
+// How Did I Not Know This TypeScript Trick Earlier??!
+// https://youtu.be/9i38FPugxB8?si=G9EBCw2mXhiQ2dMz
+
+export type act_objarr_t<t extends object, k extends keyof t, o extends t[k]> = {
+    sort?:oarr.sort_t<t,k>|undefined
+} & ({
+    type:"SORT"
+} | {
+    type:"PUSH",
+    input:t
+}
+| {
+    type:"DELETE",
+    index:number
+}
+| {
+    type:"EDIT_ATTR",
+    index:number,
+    attr:k,
+    input:o,
+}
+| {
+    type:"EDIT_ITEM",
+    index:number,
+    input:t,
+}
+| {
+    type:"SET",
+    input: t[],
+})
+
+export default function act_objarr<t extends object, k extends keyof t, o extends t[k]>
+    (prev_arr:t[], action:act_objarr_t<t,k,o>){
+    switch(action.type) { 
+        case "SORT": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_SORT_ARR = oarr.sort_arr(
+                C_COPY_ARR,
+                action.sort)
+            return C_SORT_ARR
+        } 
+        case "EDIT_ITEM": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_UPDATE = oarr.edit_item(
+                C_COPY_ARR,
+                action.index,
+                action.input
+            )
+            const C_SORT_ARR = oarr.sort_arr(
+                C_UPDATE,
+                action.sort
+            )
+            return C_SORT_ARR
+        } 
+        case "EDIT_ATTR": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_UPDATE = oarr.edit_attr(
+                C_COPY_ARR,
+                action.index,
+                action.input,
+                action.attr,
+            )
+            const C_SORT_ARR = oarr.sort_arr(
+                C_UPDATE,
+                action.sort
+            )
+            return C_SORT_ARR
+        } 
+        case "PUSH": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_UPDATE = oarr.push_arr(
+                C_COPY_ARR,
+                action.input
+            )
+            const C_SORT_ARR = oarr.sort_arr(
+                C_UPDATE,
+                action.sort
+            )
+            return C_SORT_ARR
+        } 
+        case "DELETE": { 
+            const C_COPY_ARR = [...prev_arr]
+            const C_UPDATE = oarr.delete_item(
+                C_COPY_ARR,
+                action.index
+            )
+            const C_SORT_ARR = oarr.sort_arr(
+                C_UPDATE,
+                action.sort
+            )
+            return C_SORT_ARR
+        } 
+        case "SET": {
+            const C_COPY_ARR = [...prev_arr]
+            const C_SORT_ARR = oarr.sort_arr(
+                C_COPY_ARR,
+                action.sort
+            )
+            return C_SORT_ARR
+        }
+        default: { 
+           console.log("--------------------------------------------------------------------")
+           console.log("The action.type of useObjArr is invalid.")
+           console.log("The action.type should be \"SORT\"|\"PUSH\"|\"DELETE\"|\"EDIT_ITEM\"|\"EDIT_ATTR\"|\"SET\"")
+           console.log("Warning from frontend/ src/ src/ hook/ useObjArr.tsx/ function reducer")
+           console.log("--------------------------------------------------------------------")
+           const C_COPY_ARR = [...prev_arr]
+           return C_COPY_ARR
+        } 
+    }
+}
+
+export function act_namearr<
+    t extends {name:a.name}, 
+    k extends keyof t, 
+    o extends t[k]>(prev_arr:t[], action:act_objarr_t<t,k,o> | {type:"COPY", index:number}){
+    switch (action.type){
+        case "COPY":{
+            const C_COPY_ARR = [...prev_arr]
+            const C_UPDATE = oarr.copy_unique_item(
+                C_COPY_ARR,
+                action.index
+            )
+            return C_UPDATE
+        }
+        default: {
+            if(action.sort === undefined){
+                action.sort = {
+                    attr:"name",
+                    mode:"SORT"
+                } as oarr.sort_t<t,k>
+            }
+            return act_objarr(prev_arr, action)
+        }
+    }
+}
+
+// export default function useObjArr<t extends object>(init:t[]){
+//     const [ss_objs, setss_objs] = useReducer(reducer, init);
+//     return [ss_objs, setss_objs]
+// }
+
+export type use_objarr_t<t extends object> = {
+    ss:t[],
+    setss:React.ActionDispatch<[action: act_objarr_t<t, keyof t, t[keyof t]>]>
+}
+
+export type use_namearr_t<t extends {name:a.name}> = {
+    ss:t[],
+    setss:React.ActionDispatch<[action: act_objarr_t<t, keyof t, t[keyof t]>]>
+}
