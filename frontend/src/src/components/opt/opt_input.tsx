45b6c6f2f897cd86d476f1391470c1e505b644ea
Turingraph <nemocheche@gmail.com>
2025-03-07 23:34:44 +0700

    Re[lace useState with useReducer in opt_input.tsx

--- frontend/src/src/components/opt/opt_input.tsx
+++ frontend/src/src/components/opt/opt_input.tsx
@@ -1,11 +1,11 @@
-import React, { useState, JSX, useRef, useLayoutEffect} from "react";
+import React, { useState, JSX, useRef, useLayoutEffect, useReducer} from "react";
 import * as a from "../../type/alias"
 import {STR_TO_H, str_to_optmode_arr} from "../../utility/convert";
 import { opt_mode_uit, opt_input_uit } from "./type"
 import SEARCH_BAR from "./search_bar";
 import "./index.css"
-import * as uarr from "../../utility/utility_arr";
-import { access_optmode } from "../../utility/utility";
+import { index_to_optmode } from "../../utility/convert";
+import act_objarr, { setss_namearr_t } from "../../use_reducer/act_objarr";
 
 //  https://stackoverflow.com/questions/40209352/
 //  how-to-specify-optal-default-props-with-typescript-for-stateless-functiona
@@ -28,7 +28,8 @@ export default function OPT_INPUT(
     // https://stackoverflow.com/questions/23130292/
     // test-for-array-of-string-type-in-typescript
 
-    const [ss_show_opts, setss_show_opts] = useState<opt_mode_uit[]>(
+    const [ss_show_opts, setss_show_opts] = useReducer(
+        act_objarr,
         str_to_optmode_arr(available_opts) as opt_mode_uit[]
     )
     const ref_show_opts: React.RefObject<opt_mode_uit[]|undefined> = useRef(ss_show_opts)
@@ -37,46 +38,49 @@ export default function OPT_INPUT(
     // everytime when ss_show_opt is pushed.
     useLayoutEffect(()=>{
         if(ref_show_opts.current !== ss_show_opts && auto_update_opts === true){
-        let i = 0
-        while(i < ss_show_opts.length){
-            if(ss_show_opts[i] !== undefined){
-                ss_mode.setss(ss_show_opts[i].index)
-                i = ss_show_opts.length
+            let i = 0
+            while(i < ss_show_opts.length){
+                if(ss_show_opts[i] !== undefined){
+                    ss_mode.setss(ss_show_opts[i].index)
+                    i = ss_show_opts.length
+                }
+                i+=1
             }
-            i+=1
-        }}
-        ref_show_opts.current = ss_show_opts
+            ref_show_opts.current = ss_show_opts
+        }
     },[ss_show_opts, ss_mode, auto_update_opts])
 
     // The ss_show_opts is updated every time the available_opts is updated.
     useLayoutEffect(()=>{
         if(ref_show_opts.current !== ss_show_opts && auto_update_opts === true){
-            setss_show_opts(str_to_optmode_arr(available_opts))
-            // setss_show_opts(
-            //     method_include_arr(
-            //         str_to_optmode(available_opts),
-            //         ss_show_opts)
-            //     )
+            setss_show_opts({
+                type:"SET",
+                input:str_to_optmode_arr(available_opts)
+            })
+            ref_show_opts.current = ss_show_opts
         }
-        ref_show_opts.current = ss_show_opts
     }, [available_opts, ss_show_opts, auto_update_opts])
 
     // https://stackoverflow.com/questions/40676343/
     // typescript-input-onchange-event-target-value
-    const handle_event = ((e: React.ChangeEvent<HTMLSelectElement >) => {
+    const handle_event = ((e: React.ChangeEvent<HTMLSelectElement>) => {
         ss_mode.setss(+e.target.value)
     }) as a.handle_event<HTMLSelectElement>
 
-    const JSX_OPTS = ss_show_opts.reverse().map((item,index)=>{
-        if(item !== undefined && access_optmode(item.index, str_to_optmode_arr(available_opts)) !== undefined){
+    // PREVIOUS: const JSX_OPTS = ss_show_opts.reverse().map((item,index)=>{
+    // What does .reverse() do and is it important ?
+    const JSX_OPTS = ss_show_opts.map((item,index)=>{
+        if(item !== undefined && index_to_optmode(item.index, str_to_optmode_arr(available_opts)) !== undefined){
             return (<option key={index} value={item.index}>{item.name}</option>)
         }
         return undefined
     }) as JSX.Element[]
     const JSX_SEARCH_BAR = <SEARCH_BAR
             opt_name={undefined as a.opt_name}
-            read_only_arr={str_to_optmode_arr(available_opts)}
-            setss_select_arr={setss_show_opts}
+            read_only_arr={available_opts}
+            setss_select_arr={
+                setss_show_opts as setss_namearr_t<opt_mode_uit[]>
+            }
         />
     return (<>
         <STR_TO_H opt_name={opt_name}/>
