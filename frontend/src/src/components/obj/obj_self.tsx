2b5d4f30d6876deeeaa00fdc480ff56796e03219
Turingraph <nemocheche@gmail.com>
2025-03-07 14:13:41 +0700

    modify obj_self.tsx according to use_reducer/

--- frontend/src/src/components/obj/obj_self.tsx
+++ frontend/src/src/components/obj/obj_self.tsx
@@ -1,80 +1,87 @@
-import {JSX, useState, useEffect, useLayoutEffect} from "react";
+import {JSX, useState, useEffect} from "react";
 import * as a from "../../type/alias";
 import BUTTON_CLICK from "../button/button_click";
 import INPUT_STR from "../input/input_str";
 import { STR_TO_H } from "../../utility/convert";
-import * as uarr from '../../utility/utility_arr'
 import "./index.css"
+import { use_namearr_t } from "../../use_reducer/act_objarr";
 
-export default function OBJ_SELF<t extends {name:a.name}>({
-    arr,
+export default function OBJ_SELF<
+    t extends {name:a.name}[]>({
+    input_arr,
     this_item,
     ss_select,
     jsx_additional = undefined, 
 }:{
-    arr:a.use_state_t<t[]>
+    input_arr:use_namearr_t<t>
     this_item:number
     ss_select:a.use_state_t<number|undefined>
     jsx_additional?:JSX.Element|undefined
 }){
-    const [ss_ui_mode, setss_ui_mode] = useState<"normal"|"rename"|"delete">("normal")
-    const [ss_name, setss_name] = useState<string>(arr.ss[this_item].name as string)
-    // Ideally the arr should be sorted by name. However we comment this code in order for computational efficiency.
-    // useLayoutEffect(()=>{
-    //     arr.setss(uarr.sort_arr_name(arr.ss, "SORT"))
-    // })
+    const [ss_ui_mode, setss_ui_mode] = useState<"NORMAL"|"RENAME"|"DELETE">("NORMAL")
+    const [ss_name, setss_name] = useState<string>(input_arr.ss[this_item].name as string)
     useEffect(()=>{
-        if(ss_select.ss !== this_item && ss_ui_mode !== "normal"){
-            setss_ui_mode("normal")
+        if(ss_select.ss !== this_item && ss_ui_mode !== "NORMAL"){
+            setss_ui_mode("NORMAL")
         }
     },[ss_select.ss, this_item, ss_ui_mode])
     useEffect(()=>{
-        setss_name(arr.ss[this_item].name as string)
-    },[arr.ss, this_item])
+        setss_name(input_arr.ss[this_item].name as string)
+    },[input_arr.ss, this_item])
     function func_reset(){
         ss_select.setss(undefined)
-        setss_ui_mode("normal")
+        setss_ui_mode("NORMAL")
     }
-    function func_select(ui_mode:"rename"|"delete"){
+    function func_select(ui_mode:"RENAME"|"DELETE"){
         ss_select.setss(this_item)
         setss_ui_mode(ui_mode)
     }
     function func_rename(){
-        uarr.update_item_name(
-            this_item,
-            arr,
-            ss_name,
-            "SORT"
-        )
+        input_arr.setss({
+            type:"EDIT_ATTR",
+            index:this_item as number,
+            attr:"name" as keyof t[number],
+            input:ss_name as t[number]["name"]
+        })
         func_reset()
     }
     function func_delete(){
-        uarr.delete_item(this_item, arr)
+        input_arr.setss({
+            type:"DELETE",
+            index:this_item as number
+        })
         func_reset()
     }
-    if (ss_ui_mode === "normal"){
+    function func_copy(){
+        input_arr.setss({
+            type:"COPY",
+            index:this_item as number
+        })
+        func_reset()
+    }
+    if (ss_ui_mode === "NORMAL"){
         return <>
-            <STR_TO_H opt_name={arr.ss[this_item].name as a.opt_name}/>
+            <STR_TO_H opt_name={input_arr.ss[this_item].name as a.opt_name}/>
             <BUTTON_CLICK
-                name={"rename" as a.name}
-                func_event={(()=>{func_select("rename")}) as a.func_event}
+                name={"RENAME" as a.name}
+                func_event={(()=>{func_select("RENAME")}) as a.func_event}
             />
             <BUTTON_CLICK
                 name={"copy" as a.name}
-                func_event={(()=>{uarr.copy_unique_item(this_item,arr)}) as a.func_event}
+                func_event={(()=>{func_copy()}) as a.func_event}
             />
             <BUTTON_CLICK
                 name={"x" as a.name}
-                func_event={(()=>{func_select("delete")}) as a.func_event}
+                func_event={(()=>{func_select("DELETE")}) as a.func_event}
             />
         {jsx_additional}
         </>
     }
-    else if (ss_ui_mode === "rename" && ss_select.ss === this_item){
+    else if (ss_ui_mode === "RENAME" && ss_select.ss === this_item){
         return <>
         <STR_TO_H opt_name={"rename " + ss_name + " as ?" as a.name}/>
         <INPUT_STR
-            opt_name={"rename" as a.opt_name}
+            opt_name={"RENAME" as a.opt_name}
             input={{ss:ss_name, setss:setss_name}}
         />
             <BUTTON_CLICK
@@ -88,11 +95,11 @@ export default function OBJ_SELF<t extends {name:a.name}>({
         {jsx_additional}
         </>
     }
-    else if (ss_ui_mode === "delete" && ss_select.ss === this_item){
+    else if (ss_ui_mode === "DELETE" && ss_select.ss === this_item){
         // https://stackoverflow.com/questions/15292278/
         // how-do-i-remove-an-array-item-in-typescript
         return <>
-            <STR_TO_H opt_name={"Do you want to delete \"" + arr.ss[this_item].name + "\"" as a.opt_name}/>
+            <STR_TO_H opt_name={"Do you want to delete \"" + input_arr.ss[this_item].name + "\"" as a.opt_name}/>
             <BUTTON_CLICK
                 name={"yes" as a.name}
                 func_event={(()=>{func_delete()}) as a.func_event}
