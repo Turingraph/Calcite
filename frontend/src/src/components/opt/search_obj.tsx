a9cb6db4717b5042a4493d3d144624534fe9f35e
Turingraph <nemocheche@gmail.com>
2025-03-07 23:09:49 +0700

    replace useState with useReducer in search_obj.tsx

--- frontend/src/src/components/opt/search_obj.tsx
+++ frontend/src/src/components/opt/search_obj.tsx
@@ -1,37 +1,33 @@
-import { useState, JSX, useLayoutEffect } from "react"
+import { useState, JSX, useReducer } from "react"
 import * as a from "../../type/alias"
-import { opt_mode_uit } from "./type"
 import { str_to_optmode_arr } from "../../utility/convert"
 import OBJ_SELF from "../obj/obj_self"
 import SEARCH_BAR from "./search_bar"
 import PANEL from "../asset/panel"
-import * as uarr from '../../utility/utility_arr'
+import { act_namearr, use_namearr_t } from "../../use_reducer/act_objarr"
 
-export default function SEARCH_OBJ<t extends {name:a.name}>({
-    arr,
+export default function SEARCH_OBJ<t extends {name:a.name}[]>({
+    input_arr,
     jsx_additional = undefined,
 }:{
-    arr:a.use_state_t<t[]>
+    input_arr:use_namearr_t<t>
     jsx_additional?:(JSX.Element|undefined)[]|undefined
 }){
-    const [ss_show_arr, setss_show_arr] = useState<opt_mode_uit[]>(
-        str_to_optmode_arr(arr.ss)
+    const [ss_show_arr, setss_show_arr] = useReducer(
+        act_namearr,
+        str_to_optmode_arr(input_arr.ss)
     )
     const [ss_select, setss_select] = useState<number|undefined>(undefined)
 
-    useLayoutEffect(()=>{
-        arr.setss(((prev:t[]) => uarr.sort_arr_name(prev, "SORT")) as unknown as t[])
-    })
-
     const JSX_ARR = ss_show_arr.map((item,index)=>{
-        if(item !== undefined && arr.ss[item.index] !== undefined){
+        if(item !== undefined && input_arr.ss[item.index] !== undefined){
             let jsx_item = undefined
             if (jsx_additional !== undefined && jsx_additional.length > item.index){
                 jsx_item = jsx_additional[item.index]
             }
             return <div key={index}>
                 <OBJ_SELF 
-                arr = {arr}
+                input_arr = {input_arr}
                 this_item={item.index}
                 ss_select={{ss:ss_select, setss:setss_select}}
                 jsx_additional={jsx_item ? jsx_item : <></>}
@@ -44,7 +40,7 @@ export default function SEARCH_OBJ<t extends {name:a.name}>({
     return <>
     <SEARCH_BAR 
     opt_name={"Search bar" as a.opt_name}
-    read_only_arr={arr.ss}
+    read_only_arr={input_arr.ss}
     setss_select_arr={setss_show_arr}
     />
     <PANEL jsx_element={<>{JSX_ARR}</>}/>
