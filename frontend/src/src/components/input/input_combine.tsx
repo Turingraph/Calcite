8dc7360c1e50b5844e9cfe56052a32b5b8ab8068
Turingraph <nemocheche@gmail.com>
2025-03-12 11:41:42 +0700

    Fix input_combine.tsx according to array/

--- frontend/src/src/components/input/input_combine.tsx
+++ frontend/src/src/components/input/input_combine.tsx
@@ -7,19 +7,9 @@ import OPT_INPUT from "../opt/opt_input";
 import "./index.css";
 import INPUT_FORM from "./input_form";
 
-// function func_input_opt_index(arr_all:a.attr_value<string|number>[], arr_include:string[]){
-//     return arr_all.map((item, index)=>{
-//         if(arr_include.includes(item.attr as string) === true)
-//         {
-//             return index
-//         }
-//         return 0
-//     })
-// }
-
 export type input_combine_uit = {
     opt_name?:a.opt_name,
-    input_str?:use_arrobj_t<a.attr_value<string|number>[]>|undefined
+    input_str?:use_arrobj_t<a.attr_value<string|number>[], keyof a.attr_value<string|number>>|undefined
     input_opt?:(a.attr_value<string[]> & {is_search_bar?:undefined|boolean})[]
     func_activate?:a.func_event,
     is_undo?:boolean
@@ -32,9 +22,6 @@ export default function INPUT_COMBINE({
     func_activate = (()=>{}) as a.func_event,
     is_undo = false
 }:input_combine_uit){
-    // const INPUT_OPT_INDEX = func_input_opt_index(
-    //     input_str ? input_str.ss : [], 
-    //     input_opt.map((item)=>{return item.attr}))
     let jsx_input_str = <></>
     if (input_str !== undefined){
         jsx_input_str = <INPUT_FORM
@@ -47,7 +34,7 @@ export default function INPUT_COMBINE({
     let jsx_input_opt:(JSX.Element|undefined)[]|undefined = [<></>]
     if (input_opt.length > 0 && input_str !== undefined){
         jsx_input_opt = input_opt.map((item, index:number)=>{
-            const CONST_ITEM = str_to_avarr(item.attr, input_str.ss)
+            const CONST_ITEM = str_to_avarr(item.attr, input_str.ss.ss)
             if(CONST_ITEM === undefined || typeof CONST_ITEM.value !== "number"){
                 return <div key={index}></div>
             }
