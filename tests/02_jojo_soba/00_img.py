462c19d2aac8430a951861a616518c0b46c88bfc
Turingraph <nemocheche@gmail.com>
2025-03-17 17:39:11 +0700

    Create 02_jojo_soba/00_img.py

--- tests/02_jojo_soba/00_img.py
+++ tests/02_jojo_soba/00_img.py
@@ -12,11 +12,31 @@ sys.path.append(parent)
 
 from include.img_process_gray import img_process_gray
 
-path = parent + "/tests/02_jojo_soba/img_out/img.jpg"
+path = parent + "/tests/02_jojo_soba/img/img.jpeg"
 
 img = img_process_gray(img=path)
-img.zoom(zooms=-1.5)
+img.zoom(zooms=-1.35)
 img.rotate()
 img.threshold()
-img.show()
-img.save_img()
+img.save_img(path=["img", "thresh"])
+
+img = img_process_gray(img=path)
+img.zoom(zooms=-1.35)
+img.rotate()
+img.erode()
+img.save_img(path=["img", "erode"])
+
+img = img_process_gray(img=path)
+img.zoom(zooms=-1.35)
+img.rotate()
+img.erode()
+img.threshold()
+img.save_img(path=["img", "eroth"])
+
+img = img_process_gray(img=path)
+img.zoom(zooms=-1.35)
+img.rotate()
+img.erode()
+img.threshold()
+img.thin_font()
+img.save_img(path=["img", "thinn"])
