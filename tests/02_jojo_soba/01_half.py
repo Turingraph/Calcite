f99752ebacd8df5291a495e79eeaedbb553712a4
Turingraph <nemocheche@gmail.com>
2025-03-23 17:30:01 +0700

    Update tests/02_jojo_soba/01_half.py

--- tests/02_jojo_soba/01_half.py
+++ tests/02_jojo_soba/01_half.py
@@ -1,7 +1,7 @@
 ###############################################################################################################
 
-import sys
 import os
+import sys
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -10,18 +10,22 @@ sys.path.append(parent)
 
 ###############################################################################################################
 
-from include.boxes_img import boxes_img
+# from box.box_manage import box_manage
+# from box.img_view import img_output
 import numpy as np
 
+from box.img_dilate import img_dilate
+
 path = parent + "/tests/02_jojo_soba/img/thinn.jpg"
-img = boxes_img(img = path, kernel=np.ones((13, 23)))
+img = img_dilate(img = path, kernel=np.ones((13, 23)))
+img.select_box(min_w = 1000, max_h=50)
+img.img_output().show_img(rgb=[255,0,0], title="origin")
+img.img_output().save_img(rgb=[255,0,0], path = ["img_01_half","origin"])
 
-img.select_boxes(min_w=1000,max_h=50)
-img.sort_boxes(1)
-img.show_boxes(rgb=[255,0,0], title="origin")
-img.marked_img.save_img(["img_01_half","origin"])
+box_arr = img.get_box_manage()
+box_arr.row_half(1)
+img.box = box_arr.get_box()
 
-img.row_half(1)
-img.show_boxes(rgb=[255,0,0], title="half")
-img.marked_img.save_img(["img_01_half","half"])
-img.save_boxes(["img_01_half", "subhalf"])
+img.img_output().show_img(rgb=[255,0,0], title="half")
+img.img_output().save_img(rgb=[255,0,0], path=["img_01_half","half"])
+img.img_output().save_multiple_imgs(rgb=[255,0,0], path=["img_01_half", "subhalf"])
