7b90453141dc34194a3d369c5fe905156cf3facd
Turingraph <nemocheche@gmail.com>
2025-03-23 15:36:55 +0700

    Edit 01_index/01_boxes.py

--- tests/01_index/01_boxes.py
+++ tests/01_index/01_boxes.py
@@ -1,7 +1,7 @@
 ###############################################################################################################
 
-import sys
 import os
+import sys
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
@@ -10,20 +10,14 @@ sys.path.append(parent)
 
 ###############################################################################################################
 
-from include.boxes_dilate import boxes_dilate
-from include.boxes_manage import boxes_manage
-from include.boxes_view import boxes_view
 import numpy as np
 
+from box.img_dilate import img_dilate
+
 path = parent + "/tests/01_index/img/img.jpeg"
-img = boxes_dilate(img = path, kernel=np.ones((13, 3)))
-img.select_boxes(min_w=20,min_h=200)
-
-dilate_img = img.dilate_img
-dilate_img.save_img(path="dilate_img13x3")
-
-sorted_box = boxes_manage(boxes = img.boxes)
-sorted_box.sort_boxes(method=0)
-
-img_view = boxes_view(img = img.get_img(), boxes = sorted_box.get_boxes())
-img_view.save_boxes(path = "boxes_img")
+img = img_dilate(img = path, kernel=np.ones((13, 3)))
+img.select_box(min_w=20,min_h=200)
+img.dilate_img.save_img(path="dilate_img13x3")
+img.sort_box(method=0)
+img_result = img.get_img_output()
+img_result.save_multiple_imgs(path = "boxes_img")
