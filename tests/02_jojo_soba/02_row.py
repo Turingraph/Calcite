a9b11b9346279c4cf99de2c52f145e0a4c41bc13
Turingraph <nemocheche@gmail.com>
2025-03-24 15:17:45 +0700

    Update 02_jojo_soba/02_row.py

--- tests/02_jojo_soba/02_row.py
+++ tests/02_jojo_soba/02_row.py
@@ -10,15 +10,13 @@ sys.path.append(parent)
 
 ###############################################################################################################
 
-from box.img_dilate import img_dilate
+from box.box_edit import box_edit
 import numpy as np
 
 path = parent + "/tests/02_jojo_soba/img/thinn.jpg"
-img = img_dilate(img = path, kernel=np.ones((13, 23)))
-
+img = box_edit(img = path)
+img.update_area_box(kernel=np.ones((13, 23)))
 img.select_box(min_w=1000,max_h=50)
 img.sort_box(1)
-box_arr = img.get_box_manage()
-box_arr.row_box()
-img.box = box_arr.get_box()
-img.get_img_output().save_multiple_imgs(rgb=None,path=["img_02_row", "row"])
+img.row_box()
+img.get_box_read().save_imgarr(rgb=None,path=["img_02_row", "row"])
