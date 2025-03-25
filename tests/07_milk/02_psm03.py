2118783c5175a02584e2806e258c77847f033d4a
Turingraph <nemocheche@gmail.com>
2025-03-25 09:17:00 +0700

    Create tests/07_milk/

---
+++ tests/07_milk/02_psm03.py
@@ -0,0 +1,30 @@
+###############################################################################################################
+
+import os
+import sys
+
+import numpy as np
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+parent = os.path.dirname(parent)
+sys.path.append(parent)
+
+###############################################################################################################
+
+from box.box_edit import box_edit
+
+path = "/tests/07_milk/img/"
+name = "thresh"
+format = ".jpg"
+
+img = box_edit(img = parent+path+name+format)
+img.get_ocr(
+    lang = 'eng+tha',
+    psm=3,
+    is_space=False
+)
+img.get_box_read().save_img(path="psm03")
+img.save_text(path = "psm03")
+
+###############################################################################################################
