923fb50230c9aec019bf10d0ae0fd05fac6b939c
Turingraph <nemocheche@gmail.com>
2025-03-31 12:23:22 +0700

    simplify save_path

--- basic_ocr/basic_ocr.py
+++ basic_ocr/basic_ocr.py
@@ -1,13 +1,12 @@
 import numpy as np
 from box.box_edit import box_edit
 from img_process_class.img_process_gray import img_process_gray
-from utility.utility import handle_default
 
 def get_transform_img(
         image:np.ndarray|str,
         scale:int = 1,
-        save_path:list[str]|str|bool|None = False,
-        thick_font:bool = False
+        save_path:list[str]|str|None = ["img", "img_out", "jpg"],
+        thick_font:bool = None
     ):
     img = img_process_gray(img=image)
     img.zoom(scale=scale)
@@ -15,17 +14,14 @@ def get_transform_img(
     img.threshold()
     if thick_font == True:
         img.thick_font()
-    if save_path != False:
-        img.save_img(
-            path=handle_default(
-                p=save_path,
-                d=["img", "img_out", "jpg"]))
+    if save_path != None:
+        img.save_img(path=save_path)
     return img
 
 def get_ocr(
     image:np.ndarray|str,
-    save_path_ocr:list[str]|str|bool|None = None,
-    save_path_img:list[str]|str|bool|None = False,
+    save_path_ocr:list[str]|str|None = ["text", "text", "txt"],
+    save_path_img:list[str]|str|None = ["img", "img_mark", "jpg"],
     conf:int = 60,
     lang:str = "eng",
     psm:int = 3,
@@ -48,15 +44,10 @@ def get_ocr(
         last_row=last_row,
         timeout=timeout
         )
-    if save_path_img != False:
-        img.get_box_read().save_img(
-            path=handle_default(
-                p=save_path_img,
-                d=["img", "img_out", "jpg"]))
-    if save_path_ocr != False:
-        img.save_text(path=handle_default(
-                p=save_path_ocr,
-                d=["text", "text", "txt"]))
+    if save_path_img != None:
+        img.get_box_read().save_img(path=save_path_img)
+    if save_path_ocr != None:
+        img.save_text(path=save_path_ocr)
     return img
 
 def get_box_img(
@@ -70,8 +61,8 @@ def get_box_img(
         max_w:int|None = None,
         min_h:int = 0,
         max_h:int|None = None,
-        save_path_mark:list[str]|str|bool|None = None,
-        save_path_dilate:list[str]|str|bool|None = False,
+        save_path_mark:list[str]|str|bool|None = ["img", "img_dilate", "jpg"],
+        save_path_dilate:list[str]|str|bool|None = ["img", "img_mark", "jpg"],
         rgb: list[tuple[int]] | tuple[int] | int | None = [
             [255, 0, 0], 
             [0, 255, 0], 
@@ -90,13 +81,9 @@ def get_box_img(
         max_x=max_x,
         max_y=max_y,
     )
-    if save_path_dilate != False:
-        dilate_img.save_img(path=handle_default(
-                p=save_path_dilate,
-                d=["img", "img_out", "jpg"]))
+    if save_path_dilate != None:
+        dilate_img.save_img(path=save_path_dilate)
     img.sort_box(method=0)
-    if save_path_mark != False:
-        img.get_box_read().save_imgarr(rgb=rgb,path=handle_default(
-                p=save_path_mark,
-                d=["img", "img_out", "jpg"]))
+    if save_path_mark != None:
+        img.get_box_read().save_imgarr(rgb=rgb,path=save_path_mark)
     return img
