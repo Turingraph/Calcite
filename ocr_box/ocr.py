13c796bcd1a52c7a05b5e37295af7fb325b8ddf7
Turingraph <nemocheche@gmail.com>
2025-04-03 16:48:57 +0700

    Fix get_osd() in case where image is rgb.

--- ocr_box/ocr.py
+++ ocr_box/ocr.py
@@ -1,10 +1,12 @@
 import os
 import re
 
+import cv2
 import numpy as np
 import pytesseract
 from pytesseract import Output
 
+from img_process_class.img_process_gray import img_process_gray
 from ocr_box.warning import warn_get_psm, warn_get_oem, warn_get_osd
 from utility.utility import create_dir, get_options, get_valid_path
 
@@ -50,9 +52,40 @@ def get_osd(img:np.ndarray, out_type:str = Output.STRING,timeout:int = 0)->any:
     # time : O(1) regardless of how OCR Model works
     # space: O(1)
     message = warn_get_osd()
-    out_type = get_options(input=out_type, input_options=(Output.STRING,Output.BYTES, Output.DATAFRAME, Output.DICT), message=message)
-    return pytesseract.image_to_osd(image = img, output_type=out_type,timeout=timeout)
+    out_type = get_options(
+        input=out_type, 
+        input_options=(Output.STRING,Output.BYTES, Output.DATAFRAME, Output.DICT), 
+        message=message)
+    try:
+        if len(img.shape) == 2:
+            # print("Ken",type(img),img.shape)
+            return pytesseract.image_to_osd(
+                image = img, 
+                output_type=out_type,
+                timeout=timeout)
+        elif len(img.shape) == 3:
+            img = cv2.cvtColor(src=img, code=cv2.COLOR_RGB2GRAY)
+            # print("Ken",type(img),img.shape)
+            return pytesseract.image_to_osd(
+                image = img, 
+                output_type=out_type, 
+                timeout=timeout)
+        else:
+            return "img is invalid. img should be np.ndarray where img.shape in [2, 3]."
+    except:
+        return """
+# Pleace use this command instead.
+pytesseract.image_to_osd(
+    image:      np.ndarray          = img, 
+    output_type:pytessetact.Output  = out_type, 
+    timeout:    int                 = timeout,
+    config:     str     = '--psm 0 -c min_characters_to_try=5'
+)
 
+# Read this for more details.
+# 1.    https://stackoverflow.com/questions/67920740/pytesseract-invalid-resolution-0-dpi
+# 2.    https://www.kaggle.com/code/dhorvay/pytesseract-orientation-script-detection-osd
+        """
 #-----------------------------------------------------------------------------------------
 # PURPOSE : SAVE TEXT OUTPUT
 
@@ -73,8 +106,8 @@ def save_text(
         absolute=absolute,
     )
     create_dir(path=path)
-    if os.path.isfile(os.path.join("/", *(path.split("/")))) == False:
-        open(file=path, mode="x")
+    # if os.path.isfile(os.path.join("/", *(path.split("/")))) == False:
+    #     open(file=path, mode="x")
     file = open(file=path, mode="w")
     file.write(text)
     file.close()
