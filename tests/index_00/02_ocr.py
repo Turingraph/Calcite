d99b2b0cc4b3689162df463777c76e66f04c2dc7
Turingraph <nemocheche@gmail.com>
2025-04-09 13:46:34 +0700

    Update tests/index_00/

--- tests/index_00/02_ocr.py
+++ tests/index_00/02_ocr.py
@@ -11,14 +11,24 @@ sys.path.append(parent)
 ###############################################################################################################
 
 from ocr_box.ocr_box_editor import ocr_box_editor
+from img_process_class.img_process_gray import img_process_gray
 from utility.utility import index_name
+import numpy as np
 
-path = "/tests/index_00/img/box_img_"
-file_format = ".jpg"
-
-for i in range(3):
-    img = ocr_box_editor(img = (parent + path + index_name(i) + file_format))
-    img.get_ocr(lang="eng")
-    img.save_text(path = "text/text_" + index_name(i)+".txt")
+path = parent + "/tests/index_00/img/img.jpeg"
+# It is recommended to use .threshold() before use .get_ocr() in most case.
+img_thresh = img_process_gray(img = path)
+img_thresh.threshold()
+img = ocr_box_editor(img = img_thresh.img)
+img.update_bbox(kernel=np.ones((13, 3)))
+img.select_box(min_w=20,min_h=200)
+img.sort_box(method=0)
+img.as_ocr_box_reader().show_img()
+i = 0
+for item in img.as_ocr_box_reader().get_many_imgs():
+    img_item = ocr_box_editor(img=item)
+    img_item.get_ocr(lang="eng")
+    img_item.save_text(path = "text/text_" + index_name(i)+".txt")
+    i += 1
 
 ###############################################################################################################
--- tests/index_00/README.md
+++ tests/index_00/README.md
@@ -9,4 +9,9 @@ This folder contains
 -   Purpose : Transform `img/img.jpeg` as `img/dilate_img13x3.jpg`, 
     `img/box_img_00.jpg`, `img/box_img_01.jpg` and `img/box_img_02.jpg`
 5.  `02_ocr.py`
--   Purpose : Transform `box_img_00.jpg`, `box_img_01.jpg` and `box_img_02.jpg` as text output.
+-   Purpose : Transform `img/img.jpeg` as text output.
+
+Note that it is recommended to
+1.  Processing image using `img_process_gray.threshold()` method
+2.  Avoid save `.jpeg` image and reuse it, because `.jpeg` files is compressed, which cause less reliable OCR result.
+3.  Set `get_ocr(..., conf = 0)` when after preprocessing the image to prevent information lost.
