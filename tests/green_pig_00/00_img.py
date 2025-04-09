a8a97569a07e77e1aa8621623b8400a749e8aaf3
Turingraph <nemocheche@gmail.com>
2025-04-09 13:56:42 +0700

    Update tests/green_pig_00/

--- tests/green_pig_00/00_img.py
+++ tests/green_pig_00/00_img.py
@@ -11,6 +11,7 @@ sys.path.append(parent)
 ###############################################################################################################
 
 from img_process_class.img_process_gray import img_process_gray
+from ocr_box.ocr_box_editor import ocr_box_editor
 
 path = parent + "/tests/green_pig_00/img/img.jpg"
 
@@ -19,3 +20,11 @@ img.zoom(scale=1)
 img.rotate()
 img.threshold()
 img.save_img(path="img/thresh.jpg")
+# It is not recommended to save jpeg and compute the OCR result, because jpeg file is compressed which make the
+# OCR result less reliable.
+img = ocr_box_editor(img = img.img)
+img.get_ocr(conf=50, lang="eng+tha", psm=11)
+img.as_ocr_box_reader().save_img(path= "img/mark.jpg")
+img.save_text()
+img.get_osd()
+img.save_text(path="text/osd.txt")
--- tests/green_pig_00/README.md
+++ tests/green_pig_00/README.md
@@ -4,6 +4,4 @@ This folder contains
 2.  `text/`
 -   Contains : text output.
 3.  `00_img.py`
--   Purpose :  Transform `img/img.jpeg` as `img/mark.jpg` and `img/thresh.jpg`
-4.  `01_ocr.py`
--   Purpose : Transform `img/mark.jpg` as text output and `text/text.txt` output.
+-   Purpose :  Transform `img/img.jpeg` as `img/mark.jpg`, `img/thresh.jpg`, `text/text.txt` and `text/osd.txt`
