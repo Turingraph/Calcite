557222a8883f0b74addc6e4f1ea14c3d21187e89
Turingraph <nemocheche@gmail.com>
2025-04-20 15:56:07 +0700

    Create spell_texts()

--- nlp/tha.py
+++ nlp/tha.py
@@ -1,12 +1,14 @@
 from pythainlp.tokenize import Trie, word_tokenize
 from pythainlp.corpus.common import thai_words
 from utility.utility_advance import file_to_liststr
+import re
+from pythainlp.spell import spell
 
 def sentence_to_list(
         text:str,
         custom_words:list[str] = [],
         custom_words_path:str|None = None,
-        keep_whitespace:bool = True):
+        keep_whitespace:bool = True)->list[str]:
     ls = []
     if custom_words_path != None:
         ls = file_to_liststr(custom_words_path)
@@ -21,3 +23,22 @@ def sentence_to_list(
                 ))),
         keep_whitespace=keep_whitespace,
         join_broken_num=True)
+
+def spell_texts(
+        text:str,
+        custom_words:list[str] = [],
+        custom_words_path:str|None = None,
+        keep_whitespace:bool = True)->list[str]:
+    ls = sentence_to_list(
+        text=text,
+        custom_words=custom_words,
+        custom_words_path=custom_words_path,
+        keep_whitespace=keep_whitespace
+    )
+    output = []
+    for i in ls:
+        if re.match('[A-Za-z0-9]', i) or i in "\n,.!@#$%^&*()_-+=[]:;\"\'<>?~` ":
+            output.append(i)
+        else:
+            output.append(spell(i))
+    return output
