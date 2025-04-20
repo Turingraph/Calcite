d0f9d5df829e15da8e1c496c2f7a6180622e072c
Turingraph <nemocheche@gmail.com>
2025-04-20 16:13:46 +0700

    create thai_to_arabic_digit()

--- nlp/tha.py
+++ nlp/tha.py
@@ -24,11 +24,43 @@ def sentence_to_list(
         keep_whitespace=keep_whitespace,
         join_broken_num=True)
 
+def thai_to_arabic_digit(number:str):
+    match number:
+        case "๐":
+            return "0"
+        case "๑":
+            return "1"
+        case "๒":
+            return "2"
+        case "๓":
+            return "3"
+        case "๔":
+            return "4"
+        case "๕":
+            return "5"
+        case "๖":
+            return "6"
+        case "๗":
+            return "7"
+        case "๘":
+            return "8"
+        case "๙":
+            return "9"
+        case default:
+            return number
+
+def thai_to_arabic_number_text(text:str):
+    output = ""
+    for i in text:
+        output = thai_to_arabic_digit(i)
+    return output
+
 def spell_texts(
         text:str,
         custom_words:list[str] = [],
         custom_words_path:str|None = None,
-        keep_whitespace:bool = True)->list[str]:
+        keep_whitespace:bool = True,
+        no_thai_number:bool = True)->list[str]:
     ls = sentence_to_list(
         text=text,
         custom_words=custom_words,
@@ -40,5 +72,8 @@ def spell_texts(
         if re.match('[A-Za-z0-9]', i) or i in "\n,.!@#$%^&*()_-+=[]:;\"\'<>?~` ":
             output.append(i)
         else:
-            output.append(spell(i))
+            if no_thai_number == True:
+                output.append(thai_to_arabic_number_text(spell(i)))
+            else:
+                output.append(spell(i))
     return output
